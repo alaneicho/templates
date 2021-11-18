17382779aadad5456e13fea4ae5e407f92b904c7
alaneicho <93944293+alaneicho@users.noreply.github.com>
2021-11-18 13:57:52 -0300

    Ejercicios 1-4 correctos, test pasan.

--- src/Templates.hpp
+++ src/Templates.hpp
@@ -1,13 +1,11 @@
 
-template <class T>
-
+template<class T>
 // Ejercicio 1: Pasar a templates
 T cuadrado(T x) {
     return x * x;
 }
 
 template<class Contenedor, class Elem>
-
 // Ejercicio 2: Pasar a templates
 bool contiene(Contenedor s, Elem c) {
     for (int i = 0; i < s.size(); i++) {
@@ -18,4 +16,30 @@ bool contiene(Contenedor s, Elem c) {
     return false;
 }
 
+template<class Contenedor>
 // Ejercicio 3: Funcion es prefijo con un template contenedor
+bool esPrefijo(Contenedor a, Contenedor b) {
+    if (a.size() >= b.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+template<class Contenedor, class Elem>
+// Ejercicio 4: Función maximo con un template contenedor y uno elemento
+Elem maximo(Contenedor c) {
+    Elem maximo = c[0];
+    for (int i = 0; i < c.size(); i++){
+        if (c[i] > maximo){
+            maximo = c[i];
+        }
+    }
+    return maximo;
+}
